14cde3b5a039fecafa560f26fd0d5b75058eab59
0gabriel <32753452+0gabriel@users.noreply.github.com>
2019-06-27 14:31:53 -0300

    Arquivo principal não havia sido adicionado ao git.

---
+++ app/controllers/Conexao.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc._
+
+import scala.util.parsing.json.JSON
+
+object Conexao extends App {
+  def key = "8b5a004556603b16260559b8b78120aa"
+  def urlOpening = "http://api.openweathermap.org/data/2.5/"
+
+  def weatherCity(cityName: String) =  {
+    var url = urlOpening + "weather?q="
+    url = url + cityName
+    url = url + "&appid=" + key
+    val result = scala.io.Source.fromURL(url).mkString
+    result
+  }
+
+  def uvCity(cityName: String) = {
+    val json: JsValue = Json.parse(weatherCity(cityName))
+    val longitude = (json \ "coord" \ "lon").asOpt[Double]
+    val latitude = (json \ "coord" \ "lat").asOpt[Double]
+    var url = urlOpening + "uvi?lat=" + latitude.get
+    url = url + "&lon=" + longitude.get
+    url = url + "&appid=" + key
+    val result = scala.io.Source.fromURL(url).mkString
+    result
+  }
+
+  def forecastCity(cityName: String): String = {
+    val json: JsValue = Json.parse(weatherCity(cityName))
+    val longitude = (json \ "coord" \ "lon").asOpt[Double]
+    val latitude = (json \ "coord" \ "lat").asOpt[Double]
+    var url = urlOpening + "forecast?lat=" + latitude.get
+    url = url + "&lon=" + longitude.get
+    url = url + "&appid=" + key
+    val result = scala.io.Source.fromURL(url).mkString
+    result
+  }
+
+}
